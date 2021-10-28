ffe58500967064b499065b9cda2a7d350d358767
Austin Gilmore <austingilmora@gmail.com>
2021-10-28 18:39:02 -0400

    password length choice prompt

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,22 @@ var symbolArr = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";"
 var numberArr = ["1","2","3","4","5","6","7","8","9","0"];
 
 
+function passLength() {
+    var chosenLength = window.prompt("How long do you want your password?");
+    if (parseInt(chosenLength) < 8) {
+        window.alert("Your password must be at least 8 characters long. Please try again.");
+        return null;
+    }
+    if (parseInt(chosenLength) > 128) {
+        window.alert("Your password can be no longer than 128 characters. Please try again.");
+        return null;
+    }
+    if (isNaN(parseInt(chosenLength))) {
+        window.alert("This is not a number. Please try again");
+        return null;
+    }
+    return chosenLength;
+}
 
 
 // Write password to the #password input
